32bee0ee12e67257074525734ad2b3f43bb2cf54
Chinmay Kulkarni <159155703+Chinmay1635@users.noreply.github.com>
2025-04-09 20:51:02 +0530

    Update frontend url in cors

--- backend/main.py
+++ backend/main.py
@@ -54,7 +54,7 @@ db = None
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500"],
+    allow_origins=["https://hope-red-mu.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

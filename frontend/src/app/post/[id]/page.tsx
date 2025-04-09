56bfc239536258df8f13f467e136e500e555cfb1
Chinmay Kulkarni <159155703+Chinmay1635@users.noreply.github.com>
2025-04-09 20:40:48 +0530

    Update page.tsx

--- frontend/src/app/post/[id]/page.tsx
+++ frontend/src/app/post/[id]/page.tsx
@@ -1,12 +1,14 @@
-import PostDetail from '@/components/PostDetail';
+// import PostDetail from '@/components/PostDetail';
 import React from 'react';
 
-async function Page({ params }: { params: { id: string } }) {
-  const { id } = await params;
+async function Page() {
+  // { params }: { params: { id: string } }
+  // const { id } = await params;
 
   return (
     <div className="h-full">
-      <PostDetail id={id} />
+{/*       <PostDetail id={id} /> */}
+      Post Details
     </div>
   );
 }

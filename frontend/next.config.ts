92ca2bb33d6056158ba9128cd715943f9a3a9d7f
Chinmay Kulkarni <159155703+Chinmay1635@users.noreply.github.com>
2025-04-09 16:38:56 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,16 +5,18 @@ const nextConfig: NextConfig = {
     // runtime: 'nodejs', // Removed as it is not a valid property
   },
   images: {
-   
-    domains: ['img.clerk.com'], // Allow images from any domain
+    domains: ['img.clerk.com'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'haven-user-data.s3.ap-south-1.amazonaws.com', // Allow images from any hostname
+        hostname: 'haven-user-data.s3.ap-south-1.amazonaws.com',
         port: '',
-        pathname: '/generated-images/**', // Allow any path
+        pathname: '/generated-images/**',
       },
     ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
